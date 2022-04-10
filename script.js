c045122ea6200587c2e8b23f49f8cc9d12900511
Wilef Mariety <marietyanaleticia@gmail.com>
2022-04-09 20:54:40 -0500

    mejoré la lista de contactos con html y css y añadi objetos

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 //funcion para añadir contacto
 
-function nvoContacto(){
-contactos.push(prompt("nombre y apellido del contacto"));
-}
+function nvoContacto() {
+  const contact = {
+    id: parseInt(prompt("Ingrese un número de id")),
+    nombre: prompt("Ingrese el nombre del contacto"),
+    apellido: prompt("Ingrese el apellido del contacto"),
+    telefono: parseInt(prompt("Ingrese un número de teléfono")),
+    ubicacion: [prompt("Ingrese Ciudad"), prompt("Ingrese dirección")],
+  };
 
+  contactos.push(contact);
+}
 
 //funcion para borrar contacto
 
-function borrarContacto(){
-const indice = parseInt(
-  prompt("ingrese la posicion del contacto que desea borrar")
-);    
-contactos.splice(indice,1);
-
+function borrarContacto() {
+  const indice = parseInt(
+    prompt("ingrese la posicion del contacto que desea borrar")
+  );
+  contactos.splice(indice, 1);
 }
 
-function imprimirContactos(){
-    console.log(contactos);
+//funcion para imprimir contactos
+function imprimirContactos() {
+  console.log(contactos);
 }
 
 //inico de la ejecucion
-let contactos = ["Petruzca Ramirez", "Juana Quintero ", "Johana Soto"];
+
+let contactos = [
+  {
+    id: 1,
+    nombre: "Ana ",
+    apellido: "Soto",
+    telefono: 04242210073,
+    ubicacion: ["Guarenas", "Nva Casarapa"],
+  },
+  {
+    id: 2,
+    nombre: "Leticia",
+    apellido: "Quintero",
+    telefono: 04149121344,
+    ubicacion: ["Guatire", "Calle Zamora"],
+  },
+  {
+    id: 3,
+    nombre: "Miguel",
+    apellido: "Peña",
+    telefono: 0978872443,
+    ubicacion: ["Quito", "Quitumbre"],
+  },
+];
 
 nvoContacto();
 borrarContacto();
 imprimirContactos();
-
-
